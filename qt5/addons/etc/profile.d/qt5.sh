7243e43422590a5800a1357528c8f4b3a6124656
rezso <rezso@rezso.net>
2016-08-21 16:59:01 +0200

    upd: qt5, uhubuild

--- qt5/addons/etc/profile.d/qt5.sh
+++ qt5/addons/etc/profile.d/qt5.sh
@@ -2,9 +2,3 @@ QT5DIR=/usr/lib/qt5
 
 export PATH="$PATH:$QT5DIR/bin"
 export QT5DIR
-
-if [[ ($UID = 82 || $UID = 0) && -z $PKG_CONFIG_PATH ]]; then
-PKG_CONFIG_PATH=$(pkg-config --variable pc_path pkg-config)
-export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$QT5DIR/lib/pkgconfig
-fi
-
